c2ac16e8f2ec2c61fdaaabaeab29e901ebcbdfd2
Tony Summerville <tsummerville@rarestep.com>
2014-11-26 16:22:40 -0600

    RAG widget

--- index.js
+++ index.js
@@ -166,6 +166,23 @@ Widget.prototype.funnel = function (map, callback) {
 };
 
 /**
+ * Push a RAG payload.
+ * https://developer.geckoboard.com/#rag
+ *
+ * @param {Object} map
+ * @param {Function} callback
+ */
+
+Widget.prototype.rag = function (map, callback) {
+  var payload = { item: [] };
+  Object.keys(map).forEach(function (key) {
+    var val = map[key];
+    payload.item.push({ text: key, value: val });
+  });
+  this.push(payload, callback);
+};
+
+/**
  * Push a `payload` payload to a Widget widget.
  *
  * @param {Object} payload
